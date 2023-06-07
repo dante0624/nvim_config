ae75c6bcc6be68909e0cf53a890410e2405194c4
Dante <dante.criscio@uconn.edu>
2023-06-07 19:38:14 -0400

    Added packer bootstrapping

--- lua/myPlugins.lua
+++ lua/myPlugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
 	augroup packer_user_config
@@ -182,5 +195,10 @@ return packer.startup(function(use)
 		end,
 	}
 
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
 
