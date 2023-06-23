eafb7472921ddacdd26d0ae2a529b75fd396446e
Dante <dante.criscio@uconn.edu>
2023-06-22 20:56:34 -0400

    Made Folding based on Treesitter and custom behavior

--- lua/myFolding.lua
+++ lua/myFolding.lua
@@ -1,4 +1,32 @@
-vim.o.foldmethod = "indent"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+function _G.MyFoldText()
+	local first_line = vim.fn.getline(vim.v.foldstart)
+	local last_line = vim.fn.getline(vim.v.foldend):gsub("^%s*", "") -- Removes leading whitespaces
+	local line_count = vim.v.foldend - vim.v.foldstart - 1
+
+	-- Normally include the last line in the preview
+	-- Unless it is long, then we do not
+	if string.len(last_line) > 10 then
+		line_count = line_count + 1
+		last_line = ""
+	end
+
+	local fold_message = ' +--- ' .. line_count .. ' lines ---+ '
+	if line_count == 1 then
+		fold_message = ' +--- ' .. line_count .. ' line ---+ ' -- Fix plural / singular
+	end
+
+    local fold_text =  first_line .. fold_message .. last_line
+
+	-- Turn tabs into appropriate number of spaces
+	-- Need to do this now because default behavior later is to turn one tab into one space
+	local tab_spaces = string.rep(" ", vim.o.ts)
+	return fold_text:gsub("\t",tab_spaces)
+end
+vim.opt.foldtext = 'v:lua.MyFoldText()'
+vim.opt.fillchars:append({fold = " "}) -- Gets rid of trailing dots that vim automatically adds in
 
 -- We want to only remember folds on these file types
 	-- Each call to 'mkview' creates a tmp file
