7143606439ce75b21d323c9c8431acba17c92d67
Dante Criscio <dantecriscio@gmail.com>
2023-10-29 16:12:48 -0400

    Added helpful utilities to HUD, and remapped keybinds

--- lua/core/myModules/headsUpDisplay.lua
+++ lua/core/myModules/headsUpDisplay.lua
@@ -6,14 +6,28 @@ end
 
 local M = {}
 
--- New, table oriented way of going about this
+--[[ Table oriented way of going about this
+Each Display is an interface which needs to implement:
+	isShow()
+		Return true, false, or nil
+		nil generally means that something is wrong, like a plugin is not loaded
+		When nil is returned, the calling function should generally do nothing
+	show()
+		Causes the display to be shown, even if it is already shown
+	hide()
+		Causes the display to be hidden, even if it is already hidden
+
+	show() and hide() should use protected calls to check if a necessary plugin is ready,
+	if not these functions should do nothing. ]]
 M.header = {}
 function M.header.isShown()
 	return vim.o.showtabline ~= 0
 end
+
 function M.header.show()
 	vim.opt.showtabline = 2
 end
+
 function M.header.hide()
 	vim.opt.showtabline = 0
 end
@@ -22,24 +36,28 @@ M.footer = {}
 function M.footer.isShown()
 	return vim.o.laststatus ~= 0
 end
+
 function M.footer.show()
 	vim.opt.laststatus = 2
 end
+
 function M.footer.hide()
 	vim.opt.laststatus = 0
 end
 
-M.numbers = {}
-function M.numbers.isShown()
+M.line_numbers = {}
+function M.line_numbers.isShown()
 	return vim.o.number
 end
-function M.numbers.show()
+
+function M.line_numbers.show()
 	local original_buffer = vim.fn.bufnr()
 	vim.opt.number = true
 	vim.cmd('silent! bufdo set number')
 	vim.cmd('buffer ' .. original_buffer)
 end
-function M.numbers.hide()
+
+function M.line_numbers.hide()
 	local original_buffer = vim.fn.bufnr()
 	vim.opt.number = false
 	vim.cmd('silent! bufdo set nonumber')
@@ -55,12 +73,14 @@ function M.git_signs.isShown()
 
 	return require('gitsigns.config').config.signcolumn
 end
+
 function M.git_signs.show()
 	local shown = M.git_signs.isShown()
 	if shown == false then
 		vim.cmd('silent! Gitsigns toggle_signs')
 	end
 end
+
 function M.git_signs.hide()
 	local shown = M.git_signs.isShown()
 	if shown == true then
@@ -68,15 +88,28 @@ function M.git_signs.hide()
 	end
 end
 
+M.diagnostics = {}
+function M.diagnostics.isShown()
+	return not vim.diagnostic.is_disabled()
+end
+
+function M.diagnostics.show()
+	vim.diagnostic.enable()
+end
+
+function M.diagnostics.hide()
+	vim.diagnostic.disable()
+end
+
 -- TODO:
 -- Use vim.diagnostic with 3 methods calls to optionally hide diagnostics
-	-- enable, disable, and is_diabled
+-- enable, disable, and is_diabled
 -- Wrap functions calls by saying that it is being enabled or disabled
-	-- Then make toggle call these wrapped functions
-	-- This is nice, because now toggle will tell you if it is on or off
-	-- Could be useful for diagnostics, or gitsigns where it is unclear
+-- Then make toggle call these wrapped functions
+-- This is nice, because now toggle will tell you if it is on or off
+-- Could be useful for diagnostics, or gitsigns where it is unclear
 -- Remap these so that they start with h
-	-- Then remap gitsigns so that they start with s
+-- Then remap gitsigns so that they start with s
 
 -- Give each display option certain new methods automatically
 for display_name, display in pairs(M) do
@@ -84,11 +117,13 @@ for display_name, display in pairs(M) do
 	function display.toggle()
 		local shown = display.isShown()
 		if shown == true then
+			print("Hiding: " .. display_name)
 			display.hide()
-		end
-
-		if shown == false then
+		elseif shown == false then
+			print("Showing: " .. display_name)
 			display.show()
+		else
+			print("Failed toggle call on: " .. display_name)
 		end
 	end
 
@@ -110,25 +145,56 @@ for display_name, display in pairs(M) do
 	end
 end
 
+-- Prefix with h for HeadsUpDisplay
+Map('', '<Leader>hh', M.header.toggle)
+Map('', '<Leader>hf', M.footer.toggle)
+Map('', '<Leader>hl', M.line_numbers.toggle)
+Map('', '<Leader>hs', M.git_signs.toggle)
+Map('', '<Leader>hd', M.diagnostics.toggle)
 
--- s, short for "show"
-Map('', '<Leader>sk', M.header.toggle)
-Map('', '<Leader>sj', M.footer.toggle)
-Map('', '<Leader>sl', M.numbers.toggle)
-Map('', '<Leader>sh', M.git_signs.toggle)
 
--- Open or close all display options
-Map('', '<Leader>so', function()
-	for _, display in pairs(M) do
-		display.show()
-	end
-end)
-
-Map('', '<Leader>sc', function()
+-- These functions can be used to quickly specify favorite sets of displays to show/hide
+local function onlyShow(tbl)
+	-- First hide everything
 	for _, display in pairs(M) do
 		display.hide()
 	end
+
+	for _, mode_name in ipairs(tbl) do
+		M[mode_name].show()
+	end
+end
+local function onlyHide(tbl)
+	-- First show everything
+	for _, mode in pairs(M) do
+		mode.show()
+	end
+
+	for _, mode_name in ipairs(tbl) do
+		M[mode_name].hide()
+	end
+end
+
+
+-- Show no displays
+Map('', '<Leader>hq', function()
+	onlyShow({})
+end)
+
+-- Show all displays
+Map('', '<Leader>ha', function()
+	onlyHide({})
+end)
+
+-- My own verion of "zen mode". I think its important to still show diagnostics
+Map('', '<Leader>hz', function()
+	onlyShow({'diagnostics'})
+end)
+
+-- Get rid of the header because "The Primagen" (p) suggests not using it
+-- Use this when trying to immediately jump to buffers with <Control> {a-f}
+Map('', '<Leader>hp', function()
+	onlyHide({'header'})
 end)
 
 return M
-
--- lua/plugins/gitSigns.lua
+++ lua/plugins/gitSigns.lua
@@ -8,13 +8,13 @@ local function git_on_attach(bufnr)
 	end
 
 	-- Navigation
-	map('n', '<leader>hj', function()
+	map('n', '<leader>sj', function()
 		if vim.wo.diff then return '<leader>hj' end
 		vim.schedule(function() gs.next_hunk() end)
 		return '<Ignore>'
 	end, {expr=true})
 
-	map('n', '<leader>hk', function()
+	map('n', '<leader>sk', function()
 		if vim.wo.diff then return '<leader>hk' end
 		vim.schedule(function() gs.prev_hunk() end)
 		return '<Ignore>'
@@ -22,16 +22,16 @@ local function git_on_attach(bufnr)
 
 	-- Actions
 	-- map('n', '<leader>hs', gs.stage_hunk)
-	map('n', '<leader>hr', gs.reset_hunk)
+	map('n', '<leader>sr', gs.reset_hunk)
 	-- map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line("."), vim.fn.line("v")} end)
-	map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
+	map('v', '<leader>sr', function() gs.reset_hunk {vim.fn.line("."), vim.fn.line("v")} end)
 	-- map('n', '<leader>hS', gs.stage_buffer)
 	-- map('n', '<leader>hu', gs.undo_stage_hunk)
-	map('n', '<leader>hR', gs.reset_buffer)
-	map('n', '<leader>hp', gs.preview_hunk)
-	map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+	map('n', '<leader>sR', gs.reset_buffer)
+	map('n', '<leader>sp', gs.preview_hunk)
+	map('n', '<leader>sb', function() gs.blame_line{full=true} end)
 	-- map('n', '<leader>tb', gs.toggle_current_line_blame)
-	map('n', '<leader>hd', gs.diffthis)
+	map('n', '<leader>sd', gs.diffthis)
 	-- map('n', '<leader>hD', function() gs.diffthis('~') end)
 	-- map('n', '<leader>td', gs.toggle_deleted)
 
