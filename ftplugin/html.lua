36f34e3f6f96f754f8482f903d7205a88c6fc0c5
Dante Criscio <dantecriscio@gmail.com>
2024-06-11 18:13:27 -0400

    Hotfix for ftplugin/html.lua file

--- ftplugin/html.lua
+++ ftplugin/html.lua
@@ -4,10 +4,9 @@ local folding = require("core.myModules.folding")
 
 folding.setup_treesitter_folding()
 
-require("lsp.languageCommon").start_or_attach(
+require("lsp.serverCommon").start_or_attach(
 	"htmlServer",
 	root_dir,
 	single_file
 )
-require("lsp.serverCommon").start_or_attach("vscode-html-language-server")
 require("linting.lintCommon").setup_linters({ "htmlhint" })
