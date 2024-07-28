7322d0110d0ca317e5581457ccb81c08668079d9
Dante Criscio <dantcri@amazon.com>
2024-07-28 13:03:33 -0400

    Change Java Semantic Token highlighting

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -10,6 +10,43 @@ local folding = require("core.myModules.folding")
 
 folding.setup_treesitter_folding()
 
+-- Helper function for highlighting semantic_tokens
+local function set_highlight(token, args, highlight_group)
+	vim.lsp.semantic_tokens.highlight_token(
+		token, args.buf, args.data.client_id, highlight_group
+	)
+end
+
+local custom_static_non_final = "StaticNonFinal"
+vim.cmd.highlight(custom_static_non_final, "guifg=#f7768e")
+
+vim.api.nvim_create_autocmd('LspTokenUpdate', {
+	buffer = 0,
+	callback = function(args)
+		local token = args.data.token
+
+		if token.type == "namespace" and token.modifiers.importDeclaration then
+			set_highlight(token, args, "@variable")
+		end
+
+		if token.type ~= "property" then
+			return
+		end
+
+		-- Readonly means "final" in java
+		if token.modifiers.readonly then
+			set_highlight(token, args, "Constant")
+
+		else
+			if token.modifiers.static then
+				set_highlight(token, args, custom_static_non_final)
+			else
+				set_highlight(token, args, "@property")
+			end
+		end
+	end,
+})
+
 require("lsp.serverCommon").start_or_attach(
 	"javaServer",
 	root_dir,
