ba4dffa9798d94af601957b9a955afdfe0708500
Dante Criscio <dantcri@amazon.com>
2024-07-28 23:32:08 -0400

    Bump telescope to 0.1.8 to fix strange behavior

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ local lazy_map = require("utils.map").lazy_map
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
+		tag = "0.1.8",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-fzy-native.nvim",
