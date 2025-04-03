8a2877f8198b606c1a19681e789c1f4113ef55fa
Dante Criscio <dantcri@amazon.com>
2025-04-03 14:00:50 -0400

    Change JSON files to use 2 spaces instead of tabs

--- ftplugin/json.lua
+++ ftplugin/json.lua
@@ -1,3 +1,8 @@
 local folding = require("core.myModules.folding")
 
 folding.setup_syntax_folding()
+
+-- Use 2 spaces instead of tabs (jq cli command defaults to this)
+vim.bo[0].tabstop = 2
+vim.bo[0].shiftwidth = 2
+vim.bo[0].expandtab = true
