f83766629fc0f95044ebd295fe8eedaa91a78297
Dante Criscio <dantcri@amazon.com>
2025-10-17 23:11:35 -0400

    Add back vim.fn.buflisted check to clean_no_name_buffers

--- lua/utils/buffers.lua
+++ lua/utils/buffers.lua
@@ -26,14 +26,16 @@ function M.get_buffer_ids(filter)
 end
 
 function M.clean_no_name_buffers()
-	-- Loop over all buffers, including hidden ones. Like ':ls!'
+	-- Loop over all buffers, including hidden and unlisted ones. Like ':ls!'
 	for _, n in ipairs(vim.api.nvim_list_bufs()) do
         -- Check if the buffer is safe to delete
-		if vim.fn.empty(vim.fn.bufname(n)) == 1 and -- Has no name
+		if vim.fn.buflisted(n) == 1 and -- Is already unlisted
+            vim.fn.empty(vim.fn.bufname(n)) == 1 and -- Has no name
 			vim.fn.bufwinnr(n) < 0 and -- Not in the current window
 			vim.fn.getbufvar(n, '&mod') == 0 -- Hasn't been modified
         then
-			vim.cmd('bd '..n)
+			-- Calling ` bdelete` has the effect of making a buffer "unlisted"
+			vim.cmd('bdelete '..n)
 		end
 	end
 end
